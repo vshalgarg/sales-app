a6f81a48f45137706a5ea8e52a076989115913ed
Pankaj-Dhami-007 <pankajdhami811@gmail.com>
2025-12-20 11:03:11 +0530

    properties file

--- src/main/java/com/code/monks/csm/dto/auth/response/AuthLoginResponseDto.java
+++ src/main/java/com/code/monks/csm/dto/auth/response/AuthLoginResponseDto.java
@@ -1,11 +1,13 @@
 package com.code.monks.csm.dto.auth.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.Map;
 import java.util.Set;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthLoginResponseDto {
     private Long userId;
     private String username;
