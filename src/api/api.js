968ff40a9aa0ed5f70da76e1bb18b188cafedd98
vshalgargnpl <vishalgarg@neepanlok.com>
2026-01-02 15:56:16 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 // api.js
 import axios from "axios";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
-  baseURL: environment.VITA_APP_BASE_URI
+  baseURL: BASE_URL
 });
   
 api.interceptors.request.use((config) => {
