69d230bbbcc48e7a3d8fd12edb2166c554aab690
vshalgargnpl <vishalgarg@neepanlok.com>
2026-01-02 15:43:43 +0530

    Update API base URL to use environment variable

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.100:8087/csm/api/v1",
+  baseURL: environment.VITA_APP_BASE_URI
 });
   
 api.interceptors.request.use((config) => {
