e7a6a077dfc166e99685f509f5bcbb8de1a392cc
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2026-02-21 15:12:29 +0530

    upload module enum

--- src/main/java/com/code/monks/csm/service/file/storage/FileStorage.java
+++ src/main/java/com/code/monks/csm/service/file/storage/FileStorage.java
@@ -1,8 +1,9 @@
 package com.code.monks.csm.service.file.storage;
 
+import com.code.monks.csm.enums.UploadModuleEnum;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorage {
 
-    String store(MultipartFile file, String module);
+    String store(MultipartFile file, UploadModuleEnum module);
 }
