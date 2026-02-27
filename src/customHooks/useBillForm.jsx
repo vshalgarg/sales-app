8c1c60163c03626728ab54482e6fea275cf9bcce
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2026-02-27 15:59:16 +0530

    img ui improved and error message handling in the purchase form

--- src/customHooks/useBillForm.jsx
+++ src/customHooks/useBillForm.jsx
@@ -150,32 +150,27 @@ export const useBillForm = () => {
   // }, []);
 
   // 🔹 Handlers
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+ const handleChange = (e) => {
+  const { name, value } = e.target;
 
-    let finalValue = value;
+  let finalValue = value;
 
-    // alphabets + numbers + limited special chars  
-    if (name === "order" || name === "lrNumber") {
-      finalValue = value.replace(/[^a-zA-Z0-9\s\-_/\.@#&()]/g, "");
-    }
+  if (name === "order" || name === "lrNumber") {
+    finalValue = value.replace(/[^a-zA-Z0-9\s\-_/\.@#&()]/g, "");
+  }
 
-    setFormData(prev => ({
-      ...prev,
-      [name]: finalValue,
-    }));
+  setFormData(prev => ({
+    ...prev,
+    [name]: finalValue,
+  }));
 
-    if (
-      name === "order" ||
-      name === "lrNumber"
-    ) {
-      return;
-    }
-    setErrors(prev => ({
-      ...prev,
-      [name]: validate(name, finalValue) || "",
-    }));
-  };
+  setErrors(prev => ({
+    ...prev,
+    [name]: finalValue.trim()
+      ? ""
+      : validate(name, finalValue) || "",
+  }));
+};
 
 
   const handleReset = () => {
