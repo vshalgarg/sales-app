d646552a4b10393d5d0320c81c6227482792cafb
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2026-03-13 14:45:21 +0530

    add bill item ui issue

--- src/components/BillEntry.jsx
+++ src/components/BillEntry.jsx
@@ -964,7 +964,7 @@ const BillEntry = () => {
 
         {/* Add Item Modal*/}
         {isAddItemModalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center z-50">
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div
               className="bg-white w-full h-full md:h-auto md:max-h-[80vh] md:max-w-4xl md:rounded-lg relative overflow-y-auto"
             >
