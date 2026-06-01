5b99698a2b2e63f5ee5ee2ef5e52078c861af648
Arpan Katiyar <arpankatiyar2002@gmail.com>
2026-06-01 18:17:42 +0530

    added totalAmount in table footer

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -26,6 +26,7 @@ const Credit = () => {
   const [creditHistoryData, setCreditHistoryData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [filtersApplied, setFiltersApplied] = useState(false);
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const [allSuppliers, setAllSuppliers] = useState([]);
   const [allCustomers, setAllCustomers] = useState([]);
@@ -37,11 +38,7 @@ const Credit = () => {
   const [creditToEdit, setCreditToEdit] = useState(null);
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   const [creditToDelete, setCreditToDelete] = useState(null);
-
-
   const todayDayjs = dayjs();
-  const today = dayjs().format("YYYY-MM-DD");
-
   const { errors, setErrors, filterObject, setFilterObject } = useBillForm();
 
   /* ================= LOAD SUPPLIERS & CUSTOMERS ================= */
@@ -107,6 +104,8 @@ const Credit = () => {
 
       setCreditHistoryData(data?.content ?? []);
       setTotalItems(data?.totalElements ?? 0);
+      let total=data?.totalAmount ?? 0
+      setTotalAmount(Math.round(total))
       setCurrentPage(page);
     } catch {
       setCreditHistoryData([]);
@@ -286,6 +285,7 @@ const Credit = () => {
         page={currentPage}
         totalItems={totalItems}
         rowsPerPage={rowsPerPage}
+        totalAmount={totalAmount}
         onPageChange={handleCreditHistory}
         emptyMessage={
           filtersApplied
